a31416409e077034e95049a12cc1692ce7602276
unknown <jagdeepsnh57@gmail.com>
2022-09-02 09:33:50 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+app.use(cors());
+app.options("*", cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+  transports: ["websocket", "polling"],
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId, user) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId, user);
+    socket.on("create-message", (msg, user) => {
+      io.to(roomId).emit("get-msg", msg, user);
+    });
+    socket.on("messagesended", (sended) => {
+      socket.to(roomId).emit("got-msg", sended);
+    });
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+    socket.on("mic-off", (micoff) => {
+      socket.to(roomId).emit("micoff", micoff);
+    });
+  });
+});
+
+const port = process.env.PORT || 3030;
+server.listen(port);
